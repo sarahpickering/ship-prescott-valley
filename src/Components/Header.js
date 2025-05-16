2e11601b7d325895ed384de690f7c9a3d0a883a7
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-05-16 12:36:37 -0700

    fix alignment and navbar size

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,32 @@
 import React from 'react'
+import styled from 'styled-components'
 import logo from '../assets/IMG_3721.PNG'
+import NavBar from './NavBar'
+
+const HeaderContainer = styled.header`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 10px 40px;
+    background-color: #ffffff;
+    box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+    `;
+
+    const Logo = styled.img`
+    height: 250px;
+    width: auto;
+    margin-right: 40px;
+    `;
+
+
 
 const Header = () => {
     return (
-      <header style={{ textAlign: 'center', padding: '20px' }}>
-        <img src={logo} alt="Logo" style={{ maxWidth: '200px' }} />
-      </header>
-    )
-  }
+      <HeaderContainer>
+        <Logo src={logo} alt="Ship Prescott Valley logo" />
+        <NavBar />
+      </HeaderContainer>
+    );
+  };
 
 export default Header;
