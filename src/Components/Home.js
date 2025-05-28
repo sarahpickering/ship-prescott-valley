bdc6a52b5b70c28e03a69f6794a8a50d2f59df13
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-05-28 07:36:20 -0700

    add footer, add services card

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header'
 import HeroSection from './Hero';
 import Services from './Services'
+import Footer from './Footer'
 import styled from 'styled-components';
 //import { SectionContainer, SectionTitle, SectionText } from './Styles/globalStyle';
 
@@ -12,6 +13,7 @@ function Home() {
         <>
         <HeroSection />
         <Services />
+        <Footer />
         </>
     )
 }
