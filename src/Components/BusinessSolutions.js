4f1db3e0727e164dd5f344fd35a0e5708066f260
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-06-02 12:16:18 -0700

    add shell for biz solutions

---
+++ src/Components/BusinessSolutions.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import passport from '../assets/passport.jpeg'
+import styled from 'styled-components'
+
+const BusinessSolutions = () => {
+    return (
+
+    )
+}
+
+export default BusinessSolutions;
