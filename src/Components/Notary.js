b19bd114f424f5028f0dab22061beff075a15794
Sarah Pickering <sarahpsellsaz@gmail.com>
2025-06-07 08:03:58 -0700

    update styling to match mailbox and services. move order so that what is doc notarization is on top

--- src/Components/Notary.js
+++ src/Components/Notary.js
@@ -8,13 +8,13 @@ const Container = styled.div`
     padding: 40px 20px;
     max-width: 900px;
     margin: auto;
-    background-color: #fff;
-    border: 10px solid #000b58;
+    background-color: #000b58;
+    border: 10px solid #cc0000;
 `;
 
 const Title = styled.h1`
     font-size: 2.5rem;
-    color: #000b58;
+    color: #fff;
     text-align:center;
     margin-bottom: 20px;
 `;
@@ -22,7 +22,7 @@ const Title = styled.h1`
 const Intro = styled.p`
     font-size: 1.1rem;
     line-height: 1.6;
-    color: #555;
+    color: #fff;
     text-align: center;
     margin-bottom: 40px;
 `;
@@ -30,7 +30,7 @@ const Intro = styled.p`
 
 const MoreInfo = styled.p`
   font-size: 1.1rem;
-  color: #444;
+  color: #fff;
   text-align: center;
   margin: 30px 0;
 `;
@@ -43,7 +43,7 @@ const SubtitleWrapper = styled.div`
 const SubTitle = styled.h3`
   font-weight: 900;
   font-size: 1.6rem;
-  color: #222;
+  color: #cc0000;
   margin-bottom: 15px;
   letter-spacing: 1.5px;
   border-bottom: 3px solid #000b58;
@@ -64,6 +64,16 @@ const Notary = () => {
             </Intro>
 
             <SubtitleWrapper>
+            <SubTitle>What Is Document Notarization? </SubTitle>
+            </SubtitleWrapper>
+          
+            <MoreInfo>
+            Notarization means your documents are officially certified by a notary public.<br /> 
+            The notary’s role is to confirm the identity of everyone signing, witness the signing process, <br />
+            and apply an official stamp or seal to your documents. 
+            </MoreInfo>
+
+            <SubtitleWrapper>
             <SubTitle>Our Notary Services Cover: </SubTitle>
             </SubtitleWrapper>
 
@@ -77,18 +87,11 @@ const Notary = () => {
             <MoreInfo>
             Please bring your documents unsigned—no signatures beforehand—<br />and a valid 
             photo ID for each person requiring notarization. 
-            </MoreInfo>
-
-            <SubtitleWrapper>
-            <SubTitle>What Is Document Notarization? </SubTitle>
-            </SubtitleWrapper>
-          
-            <MoreInfo>
-            Notarization means your documents are officially certified by a notary public.<br /> 
-            The notary’s role is to confirm the identity of everyone signing, witness the signing process, <br />
-            and apply an official stamp or seal to your documents. <br /><br />*Notaries do not provide legal advice 
+            <br /><br />*Notaries do not provide legal advice 
             or interpret the content of your documents.*
             </MoreInfo>
+
+            
             {/* </Content> */}
         </Container>
        
